b87d5d84fd9c18592fcc236c040278267969c1b4
10151939_wgf <10151939wang_gaofeng@cn.tre-inc.com>
2019-10-28 17:09:40 +0800

    筛选条

--- src/views/idpos.vue
+++ src/views/idpos.vue
@@ -4,13 +4,28 @@
     <!--头部-->
     <pageHeader></pageHeader>
     <!--内容容器-->
+    <div class="idposButtonDiv">
+      <div class="ztreeHeader">项目列表</div>
+      <div class="mainHeader">
+        <el-row>
+          <el-col :span="4" class="mainHeader-contentPosition">项目</el-col>
+          <el-col :span="2" class="mainHeader-contentPosition">显示</el-col>
+          <el-col :span="14" class="mainHeader-contentPosition">条件</el-col>
+          <el-col :span="4" class="mainHeader-contentPosition">收藏</el-col>
+        </el-row>
+      </div>
+    </div>
     <el-container>
       <!--左侧菜单栏-->
       <el-aside>
         <zTreeMenu></zTreeMenu>
       </el-aside>
       <!--右侧主要内容-->
-      <el-main>main</el-main>
+      <el-main>
+        <selectOption class="textPosition" 
+          v-for="item in otpion" :key="item.id">
+        </selectOption>
+      </el-main>
     </el-container>
     <!--页脚-->
     <pageFooter></pageFooter>
@@ -18,18 +33,52 @@
 </template>
 
 <script>
-import zTreeMenu from '@/viewsCompoments/ZTreeMenu.vue'
+import zTreeMenu from '@/viewsCompoments/idpos/ZTreeMenu.vue'
+import selectOption from '@/viewsCompoments/idpos/OptionForSelectModule.vue'
 
 export default {
   name:'idpos',
   components:{
     //存放该页面特有的组件
-    "zTreeMenu":zTreeMenu
+    "zTreeMenu":zTreeMenu,
+    "selectOption":selectOption
+  },
+  data:function(){
+    return {
+      otpion:[{"id":"test","type":"test"}]
+    }
   }
 }
 </script>
 <style scoped>
   .el-aside{
     border-right:#E7E7E7 solid 1px;
+  }
+  .el-main{
+    margin:0;
+    padding:0;
+  }
+  .idposButtonDiv{
+    background-color:#ECF0F1;
+    width:100%;
+    height:35px;
+  }
+  .ztreeHeader{
+    width:280px;
+    line-height: 35px;
+    text-align: left;
+    padding-left:20px;
+    float:left;
+  }
+  .mainHeader{
+    margin-left:300px;
+  }
+  .mainHeader-contentPosition{
+    text-align: left;
+    line-height: 35px;
+  }
+  .textPosition{
+    text-align: left;
+    line-height: 40px;
   }
 </style>
---
+++ src/viewsCompoments/idpos/OptionForSelectModule.vue
@@ -0,0 +1,47 @@
+<template>
+      <el-row style="background-color:#D4DEFF" class="optionBorder">
+            <el-col :span="4" class="mainHeader-contentPosition">
+                  <el-checkbox v-model="divShowOrHide" style="padding-left:5px;">测试1</el-checkbox>
+            </el-col>
+            <el-col :span="2" class="mainHeader-contentPosition">
+                  <el-checkbox v-model="optionShowOrHide"></el-checkbox>
+            </el-col>
+            <el-col :span="18" class="mainHeader-contentPosition">
+                  <el-select v-model="value" filterable placeholder="请选择" size="small">
+                        <el-option
+                              v-for="item in options"
+                              :key="item.value"
+                              :label="item.label"
+                              :value="item.value">
+                        </el-option>
+                  </el-select>
+                  <el-button type="text" style="margin-left:20px;">复数选择</el-button>
+                  <el-button type="text" style="margin-left:20px;">清空选择</el-button>
+            </el-col>
+      </el-row>
+</template>
+
+<script>
+export default {
+      data:function(){
+            return {
+                  divShowOrHide:true,
+                  optionShowOrHide:true,
+                  options: [
+                        {value: '选项1',label: '黄金糕'}, 
+                        {value: '选项2',label: '双皮奶'}, 
+                        {value: '选项3',label: '蚵仔煎'}, 
+                        {value: '选项4',label: '龙须面'}, 
+                        {value: '选项5',label: '北京烤鸭'}
+                  ],
+                  value: ''                 
+            }
+      }
+}
+</script>
+<style>
+.optionBorder{
+      border-top:#ffffff 1px solid;
+      border-bottom:#ffffff 1px solid;
+}
+</style>
